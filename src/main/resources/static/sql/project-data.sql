fbc8f06dc58bd5eb72d3de45ac9d3f3c0f07859a
staskowsky <staskowsky@gmail.com>
2019-05-21 15:43:39 +0200

    teraz plik sql tworzy także bazę danych

--- src/main/resources/static/sql/project-data.sql
+++ src/main/resources/static/sql/project-data.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `project-manager-600` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci */;
+USE `project-manager-600`;
 -- MySQL dump 10.13  Distrib 8.0.14, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: project-manager-600
@@ -14,6 +16,64 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(60) COLLATE utf8_polish_ci NOT NULL,
+  `role` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `username` varchar(60) COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_r43af9ap4edm43mmtq01oddj6` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'$2a$10$UobSnw8qHrkdK5i55nmFnuX384ZGNtDHC.hAezdbSk4mc8HwbzKZK','PROGRAMMER','Sebastian'),(2,'$2a$10$aFLmHy1J4Fel2BWA46cjW.4cVzLpztPri0WOPYJXvFnL3JV3b/c0S','PROGRAMMER','Mariusz'),(3,'$2a$10$XLP3RJPUys/QFC3Ep6MDL.ocI0/G5hKnSDg/D5sIuCPx.WGWLSBWi','PROJECT_MANAGER','Kazimiera');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tasks` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `deadline_date` date NOT NULL,
+  `description` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `start_date` date NOT NULL,
+  `status` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `assignee_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKekr1dgiqktpyoip3qmp6lxsit` (`assignee_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (1,'2019-05-23','Zadanie Sebastiana.',NULL,'Pierwsze zadanie','2019-05-19','TO_DO',1),(2,'2019-05-30','Zadanie Mariusza.',NULL,'Drugie zadanie','2019-05-19','IN_PROGRESS',2),(3,'2019-05-30','Zadanie Sebastiana utworzone przez Mariusza.',NULL,'Trzecie zadanie','2019-05-12','TO_DO',1),(4,'2019-05-16','Zadanie Mariusza utworzone przez Kazimiere.','2019-05-09','Ukończone zadanie','2019-04-30','DONE',2);
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `comments`
@@ -67,63 +127,6 @@ INSERT INTO `logs` VALUES (1,'Użytkownik Sebastian stworzył nowe zadanie: Pier
 /*!40000 ALTER TABLE `logs` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `tasks`
---
-
-DROP TABLE IF EXISTS `tasks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `tasks` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `deadline_date` date NOT NULL,
-  `description` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  `end_date` date DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  `start_date` date NOT NULL,
-  `status` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  `assignee_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKekr1dgiqktpyoip3qmp6lxsit` (`assignee_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tasks`
---
-
-LOCK TABLES `tasks` WRITE;
-/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (1,'2019-05-23','Zadanie Sebastiana.',NULL,'Pierwsze zadanie','2019-05-19','TO_DO',1),(2,'2019-05-30','Zadanie Mariusza.',NULL,'Drugie zadanie','2019-05-19','IN_PROGRESS',2),(3,'2019-05-30','Zadanie Sebastiana utworzone przez Mariusza.',NULL,'Trzecie zadanie','2019-05-12','TO_DO',1),(4,'2019-05-16','Zadanie Mariusza utworzone przez Kazimiere.','2019-05-09','Ukończone zadanie','2019-04-30','DONE',2);
-/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `password` varchar(60) COLLATE utf8_polish_ci NOT NULL,
-  `role` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  `username` varchar(60) COLLATE utf8_polish_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_r43af9ap4edm43mmtq01oddj6` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'$2a$10$UobSnw8qHrkdK5i55nmFnuX384ZGNtDHC.hAezdbSk4mc8HwbzKZK','PROGRAMMER','Sebastian'),(2,'$2a$10$aFLmHy1J4Fel2BWA46cjW.4cVzLpztPri0WOPYJXvFnL3JV3b/c0S','PROGRAMMER','Mariusz'),(3,'$2a$10$XLP3RJPUys/QFC3Ep6MDL.ocI0/G5hKnSDg/D5sIuCPx.WGWLSBWi','PROJECT_MANAGER','Kazimiera');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -134,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-14 14:10:01
+-- Dump completed on 2019-05-21 15:37:57
